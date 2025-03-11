3b884b97c1a93bcaea75112d8b0ef6d360f36d70
illustermin <2002hongmj@naver.com>
2025-03-11 16:09:15 +0900

    0311commit

--- baekjoon/js/그래프와 순회/1012_유기농배추.js
+++ baekjoon/js/그래프와 순회/1012_유기농배추.js
@@ -1,6 +1,14 @@
 const fs = require('fs');
 const input = fs.readFileSync('personal.txt').toString().trim().split("\n");
-const N = parseInt(input[0]);
-let houses = input.slice(1).map((row) => row.split("").map((num) => +num));
+const T = parseInt(input[0]);
+const [M, N, K] = input[1].split(" ").map(Number);
+
+for (let i = 0; i < M; i++) {
+    for (let j = 0; j < N; j++) {
+    
+    }
+}
+
+let houses = input.slice(2).map((row) => row.split("").map((num) => +num));
 let housesNeighborhood = [];
 
