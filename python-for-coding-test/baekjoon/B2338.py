42e1f6276dd00608284282afd23361ca825cfe3b
Minju 홍민주 <134802163+illustermin@users.noreply.github.com>
2025-03-27 17:39:11 +0900

    Delete python-for-coding-test/baekjoon directory

--- python-for-coding-test/baekjoon/B2338.py
+++
@@ -1,6 +0,0 @@
-import sys
-a,b = list(map(int, sys.stdin.read().split()))
-
-print(a+b)
-print(a-b)
-print(a*b)
--- python-for-coding-test/baekjoon/B5717.py
+++
@@ -1,8 +0,0 @@
-import sys
-data = sys.stdin.read().strip().split("\n")
-
-for line in data:
-  a,b = map(int, line.split())
-  if a==0 and b==0:
-    break
-  print(a+b)
