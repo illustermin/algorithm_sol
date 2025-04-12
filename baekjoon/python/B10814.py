1b3431a6064923e4177699a6691030a11a97eff7
illustermin <2002hongmj@naver.com>
2025-04-12 17:28:28 +0900

    add: 0412.baekjoon Q:sort

---
+++ baekjoon/python/B10814.py
@@ -0,0 +1,8 @@
+N = int(input())
+people=[]
+for _ in range(N):
+    age, name = input().split()
+    people.append((int(age), name))
+people.sort(key=lambda x: x[0])
+for age, name in people:
+    print(age, name)
